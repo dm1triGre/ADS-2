783f571a227b15d5fee2f21e3f69e7f8126249c9
anton <ashtanyuk@gmail.com>
2025-03-02 12:15:13 +0300

    Правка заглушек

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,25 @@
 
 
 double pown(double value, uint16_t n) {
-
+  return 0.0;
 }
 
 uint64_t fact(uint16_t n) {
-
+  return 0.0;
 }
 
 double calcItem(double x, uint16_t n) {
-
+  return 0.0;
 }
 
 double expn(double x, uint16_t count) {
-
+  return 0.0;
 }
 
 double sinn(double x, uint16_t count) {
-
+  return 0.0;
 }
 
 double cosn(double x, uint16_t count) {
-
+  return 0.0;
 }
