56bde8eedc233eadaaa46d88ebcfde5ecb75e79d
dm1triGre <dmitrijgrechin@gmail.com>
2025-05-13 19:57:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,49 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+  double result = 1.0;
+  for (uint16_t i = 0; i < n; ++i) {
+    result *= value;
+  }
+  return result;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  uint64_t result = 1;
+  for (uint16_t i = 2; i <= n; ++i) {
+    result *= i;
+  }
+  return result;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+  return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+  double result = 0.0;
+  for (uint16_t i = 0; i < count; ++i) {
+    result += calcItem(x, i);
+  }
+  return result;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+  double result = 0.0;
+  for (uint16_t i = 0; i < count; ++i) {
+    int sign = (i % 2 == 0) ? 1 : -1;
+    uint16_t power = 2 * i + 1;
+    result += sign * pown(x, power) / fact(power);
+  }
+  return result;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+  double result = 0.0;
+  for (uint16_t i = 0; i < count; ++i) {
+    int sign = (i % 2 == 0) ? 1 : -1;
+    uint16_t power = 2 * i;
+    result += sign * pown(x, power) / fact(power);
+  }
+  return result;
 }
