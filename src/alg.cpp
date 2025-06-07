b99e6aef38a7d5aa09d7a6765b07609aff639cff
dm1triGre <dmitrijgrechin@gmail.com>
2025-06-07 13:14:13 +0300

    b87

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,7 +2,6 @@
 #include <cstdint>
 #include "alg.h"
 
-
 double pown(double value, uint16_t n) {
   double result = 1.0;
   for (uint16_t i = 0; i < n; ++i) {
@@ -24,29 +23,27 @@ double calcItem(double x, uint16_t n) {
 }
 
 double expn(double x, uint16_t count) {
-  double result = 0.0;
-  for (uint16_t i = 0; i < count; ++i) {
-    result += calcItem(x, i);
+  double sum = 0.0;
+  for (uint16_t n = 0; n < count; ++n) {
+    sum += calcItem(x, n);
   }
-  return result;
+  return sum;
 }
 
 double sinn(double x, uint16_t count) {
-  double result = 0.0;
-  for (uint16_t i = 0; i < count; ++i) {
-    int sign = (i % 2 == 0) ? 1 : -1;
-    uint16_t power = 2 * i + 1;
-    result += sign * pown(x, power) / fact(power);
+  double sum = 0.0;
+  for (uint16_t n = 0; n < count; ++n) {
+    int sign = (n % 2 == 0) ? 1 : -1;
+    sum += sign * pown(x, 2 * n + 1) / fact(2 * n + 1);
   }
-  return result;
+  return sum;
 }
 
 double cosn(double x, uint16_t count) {
-  double result = 0.0;
-  for (uint16_t i = 0; i < count; ++i) {
-    int sign = (i % 2 == 0) ? 1 : -1;
-    uint16_t power = 2 * i;
-    result += sign * pown(x, power) / fact(power);
+  double sum = 0.0;
+  for (uint16_t n = 0; n < count; ++n) {
+    int sign = (n % 2 == 0) ? 1 : -1;
+    sum += sign * pown(x, 2 * n) / fact(2 * n);
   }
-  return result;
+  return sum;
 }
