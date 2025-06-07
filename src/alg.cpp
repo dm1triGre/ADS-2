807f88315eac0f55133af457682a377902f08ccd
dm1triGre <dmitrijgrechin@gmail.com>
2025-06-07 14:52:21 +0300

    b87

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,6 +3,7 @@
 #include "alg.h"
 
 double pown(double value, uint16_t n) {
+  if (n == 0) return 1.0;
   double result = 1.0;
   for (uint16_t i = 0; i < n; ++i) {
     result *= value;
@@ -11,6 +12,7 @@ double pown(double value, uint16_t n) {
 }
 
 uint64_t fact(uint16_t n) {
+  if (n == 0) return 1;
   uint64_t result = 1;
   for (uint16_t i = 2; i <= n; ++i) {
     result *= i;
@@ -23,11 +25,12 @@ double calcItem(double x, uint16_t n) {
 }
 
 double expn(double x, uint16_t count) {
+  if (count == 0) return 0.0;
   double sum = 0.0;
   for (uint16_t n = 0; n < count; ++n) {
     sum += calcItem(x, n);
   }
-  return sum;
+   return sum;
 }
 
 double sinn(double x, uint16_t count) {
